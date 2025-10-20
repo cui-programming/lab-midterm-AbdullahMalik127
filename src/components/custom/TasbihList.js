366d962023580c863a32fdc0fcfbbf6dcf958190
AbdullahMalik127 <abdullahmalik.9801@gmail.com>
2025-10-20 13:25:04 +0500

    commiting tasbih list

--- src/components/custom/TasbihList.js
+++ src/components/custom/TasbihList.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { View, FlatList, Text } from 'react-native'; // You may switch Text to ui/Text later
 import { styles } from '../../styles/styles';
 import { initialAzkaar } from '../../data/azkaar';
+import {Button} from '../ui';
 
 /**
  * Custom/TasbihList
@@ -13,18 +14,39 @@ export default function TasbihList() {
   const [items, setItems] = useState(initialAzkaar);
 
   // HINT ONLY (do not complete): you will need handlers like increment(id) / decrement(id)
+    
+  const increment = (id) => {
+    setItems(prev =>
+      prev.map(item =>
+        item.id === id ? { ...item, count: item.count + 1  } : item
+      )
+    );
+  };
+
+ 
+  const decrement = (id) => {
+    setItems(prev =>
+      prev.map(item =>
+        item.id === id
+          ? { ...item, count: Math.max(0, item.count - 1) }
+          : item
+      )
+    );
+  };
 
   const renderItem = ({ item }) => (
     <View style={styles.itemRow}>
       <Text style={styles.itemName}>{item.phrase}</Text>
       <Text style={styles.counter}>{item.count}</Text>
       {/* TODO: Add increment/decrement buttons here using ui/Button */}
+      <Button style={styles.button} title="+" onPress={() => {increment(item.id)}} />
+      <Button style={styles.button} title="-" onPress={() => {decrement(item.id)}} />
     </View>
   );
 
   return (
     <View style={styles.section}>
-      <Text style={styles.sectionTitle}>Tasbih Counter</Text>
+      <Text style={[styles.headerText ]}>Tasbih Counter</Text>
       <FlatList
         data={items}
         keyExtractor={(it) => it.id}
