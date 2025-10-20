627c634ccfe325418d6a6aeeb01bd47c8aee2680
AbdullahMalik127 <abdullahmalik.9801@gmail.com>
2025-10-20 12:53:07 +0500

    Add TeacherMessage component for displaying messages

---
+++ src/components/custom/TeacherMessage.js
@@ -0,0 +1,15 @@
+import { StyleSheet, View } from 'react-native'
+import React from 'react'
+import { Text } from '../ui'
+import { styles } from '../../styles/styles'
+
+const TeacherMessage = () => {
+    return (
+        <View>
+            <Text style={styles.TeacherMessage} >TeacherMessage : Work hard you will succeed ...</Text>
+
+        </View>
+    )
+}
+
+export default TeacherMessage
