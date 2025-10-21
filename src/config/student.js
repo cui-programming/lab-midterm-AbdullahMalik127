f6970472be164244b3fae1eb603cb127ef89dda8
AbdullahMalik127 <abdullahmalik.9801@gmail.com>
2025-10-20 22:48:42 -0700

    Remove placeholder comments in student.js
    Removed placeholder comments for student details.

--- src/config/student.js
+++ src/config/student.js
@@ -1,4 +1,3 @@
-// Edit these two fields with your details before you start.
-// They will be shown at the very top of the screen.
+
 export const STUDENT_NAME = "Abdullah Taj";
 export const REG_NO = "Fa22-bse-035";
